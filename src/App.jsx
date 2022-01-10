9ee70fc8492eae5ffd25a8f80cfcf85356a694f4
Jose Luis da Costa Xavier <jlcxavier@pop-os.localdomain>
2022-01-10 16:05:25 -0300

    update task info

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
@@ -56,17 +57,25 @@ const App = () => {
 	// };
 
 	return (
-		<>
-			<div className='container'>
+		<Router>
+			<div className="container">
 				<Header />
-				<AddTask handleTaskAddition={handleTaskAddition} />
-				<Tasks
-					tasks={tasks}
-					handleTaskClick={handleTaskClick}
-					handleTaskDeletion={handleTaskDeletion}
+				<Route
+					path="/"
+					exact
+					render={() => (
+						<>
+							<AddTask handleTaskAddition={handleTaskAddition} />
+							<Tasks
+								tasks={tasks}
+								handleTaskClick={handleTaskClick}
+								handleTaskDeletion={handleTaskDeletion}
+							/>
+						</>
+					)}
 				/>
 			</div>
-		</>
+		</Router>
 	);
 };
 
